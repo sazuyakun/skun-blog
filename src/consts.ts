cbbfbdbaae9a51345c4198f9332f8cbe30a65f1a
Soham Samal <sohamsamal37@gmail.com>
2026-07-30 00:01:43 +0530

    Add social icons and tighten article layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,20 @@
 export const SITE_TITLE = 'Skun Blog';
 export const SITE_DESCRIPTION = 'A personal blog.';
+
+export const SOCIALS = [
+	{
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/sazuyakun',
+	},
+	{
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/soham-samal/',
+	},
+	{
+		name: 'X',
+		icon: 'x',
+		url: 'https://x.com/sazuya_kun',
+	},
+] as const;
